a645a3e1da4218bfe9c9f70e1d1c3063b783e34a
choupc <wongci63352@yahoo.com.tw>
2014-07-10 22:36:54 +0800

    Update leds.c

--- app/leds.c
+++ app/leds.c
@@ -6,13 +6,13 @@
 int main(int argc, char **argv)
 {
 	int fd;
-	fd = open("/dev/tq2440_7seg", 0);
+	fd = open("/dev/tq2440_7seg", 0);	//開啟七段顯示器的driver
 
-	if(fd < 0){
+	if(fd < 0){				//開啟失敗
 		perror("open device 7seg error");
 		exit(1);
 	}
-	printf("hello hungyi\n");
+	printf("hello hungyi\n");		//開啟成功
 
 	for(;;);
 
